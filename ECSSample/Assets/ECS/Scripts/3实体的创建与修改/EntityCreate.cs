b4965347b09c964aacda08cf80f1a80c456fb16c
ThreeIce <THreeIce@outlook.com>
2020-01-13 12:19:48 +0800

    更新至第四课

---
+++ ECSSample/Assets/ECS/Scripts/3实体的创建与修改/EntityCreate.cs
@@ -0,0 +1,191 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Collections;
+
+
+
+//前言：
+//离上一次ECS教程更新过去了很久了，ECS飞速迭代，十一月中旬推出了0.2，十一月底（又或是十二月初）推出了0.3
+//变化极其之大，0.1到0.2淘汰了一堆东西，又新增了一堆东西（但是0.2到0.3只增加了一个功能，官方在想啥）
+//举个例子，以前通过World.Active可以获得运行的一个World，现在被废弃了，改成了World.AllWorlds（感觉是个好事）
+//注意，0.3只支持2019.3f
+
+
+
+
+
+
+
+
+/// <summary>
+/// 最基本的组件类型
+/// </summary>
+public struct TestComponentData : IComponentData
+{
+    public int Value;
+}
+/// <summary>
+/// 组件数据共享的组件类型，每个entity只拥有一个引用，一些较大的重复性数据用这个就不需要每个Entity都存了
+/// </summary>
+public struct TestSharedComponentData : ISharedComponentData
+{
+    public int Value;
+}
+/// <summary>
+/// Entity的集合组件类型
+/// </summary>
+public struct TestBufferElementData : IBufferElementData
+{
+    public int Value;
+}
+/// <summary>
+/// 特殊的ComponentData，具有在Entity删除时保留下来的特性
+/// </summary>
+//SharedComponentData和BufferElementData也有SystemState版本，操作方法类似不多讲了
+public struct TestSystemStateComponentData : ISystemStateComponentData
+{
+    public int Value;
+}
+
+public class TestClass : IComponentData
+{
+    public int Value = 1;
+}
+
+public class EntityCreate : MonoBehaviour
+{
+    public World world;
+    public EntityManager EntityManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //由于现在在MonoBehaviour里执行代码，MonoBehaviour并不属于任何一个世界，没有对任何一个世界的引用
+        //所以必须手动选择一个要操作的世界，在这个世界中生成Entity
+        world = World.AllWorlds[0];//默认情况下，unity会在开始运行时自动给我们创建一个默认世界
+        //没创建其它世界的话这个列表里就只会有这一个世界。
+        EntityManager = world.EntityManager;//获得该世界的EntityManager，通过EntityManager对世界中的实体操作
+        Create1();
+        Entity e = Create4(Create3());
+        GetAndSet(e);
+    }
+    /// <summary>
+    /// 第一种创建entity的方式
+    /// </summary>
+    public Entity Create1()
+    {
+        //通过EntityArchetype创建Entity
+        //EntityArchetype类似于没有具体组件数值的Prefab，通过它生成的实体的组件的值都是结构体的默认数值
+        //EntityArchetype的创建需要指定该Archetype有什么组件类型，组件的类型可以通过ComponentType.ReadWrite获取
+        EntityArchetype ea = EntityManager.CreateArchetype(ComponentType.ReadWrite<TestComponentData>(),
+            ComponentType.ReadWrite<TestSharedComponentData>(),
+            ComponentType.ReadWrite<TestBufferElementData>(),
+            ComponentType.ReadWrite<TestSystemStateComponentData>());
+        //此处使用ReadWrite和ReadOnly没有区别，但不清楚以后会不会有区别，推荐都使用readwrite
+        return EntityManager.CreateEntity(ea);
+    }
+    /// <summary>
+    /// 第二种创建entity的方式
+    /// </summary>
+    public Entity Create2()
+    {
+        //直接通过ComponentType创建Entity，EntityManager会在内部自己创建一个Archetype
+        return EntityManager.CreateEntity(ComponentType.ReadWrite<TestComponentData>(),
+            ComponentType.ReadWrite<TestSharedComponentData>(),
+            ComponentType.ReadWrite<TestBufferElementData>(),
+            ComponentType.ReadWrite<TestSystemStateComponentData>());
+    }
+    /// <summary>
+    /// 第三种创建方式，兼如何往一个实体上添加组件
+    /// </summary>
+    public Entity Create3()
+    {
+        //创建一个空实体，并往其上添加组件
+        Entity e = EntityManager.CreateEntity();
+        //这种方法可以往实体上添加设定好数值的组件，但是速度比其他方法会稍慢一些，推荐配合第四个方法使用
+        EntityManager.AddComponentData<TestComponentData>(e, new TestComponentData() { Value = 1 });
+        //这里的泛型由于类型推测是可以省略的，我就不打了
+        EntityManager.AddSharedComponentData(e, new TestSharedComponentData() { Value = 1 });
+        //ISystemStateComponentData继承自IComponentData，所以直接把它当作ComponentData用就行了，只是有些特性
+        EntityManager.AddComponentData(e, new TestSystemStateComponentData() { Value = 1 });
+        //上面三个函数返回值为bool，bool的意思是如果目标实体上没有同类型组件，那么返回true并添加该组件到实体上
+        //如果有，返回false并不进行操作。ECS是不支持同个Entity拥有多个同类型组件的
+
+        //Buffer的创建比较特殊
+        DynamicBuffer<TestBufferElementData> buffer = EntityManager.AddBuffer<TestBufferElementData>(e);
+        //可以通过该函数往buffer里加入内容
+        buffer.Add(new TestBufferElementData { Value = 1 });
+        //Buffer内容的删除也很特殊，官方只支持了删除指定index的对应内容的函数
+        //如buffer.RemoveAt(0);
+        //Buffer可以当成普通List来用
+        Debug.Log(buffer[0].Value);
+
+        //在这里顺便补充个特殊组件类型，第一个视频里没讲，因为它其实并不是ecs应该有的功能
+        //是为了兼容GameObject做的妥协，用这玩意性能会偏差，但总体比GameObject会好一些，最好不要多用
+        //这个函数的泛型支持继承自object的所有类，但是经过实测，需要使类被注册到unity ECS的一个TypeManager里
+        //在0.3里最方便的方法是使这个类继承自IComponentData，另外继承自MonoBehaviour的一切类都是支持的
+        EntityManager.AddComponentObject(e,new TestClass());
+        return e;
+    }
+    /// <summary>
+    /// 第四种创建方法
+    /// </summary>
+    public Entity Create4(Entity Prefab)
+    {
+        //可以复制一个现有实体的所有组件数据来创建一个新实体，和GameObject.Instantiate差不多
+        return EntityManager.Instantiate(Prefab);
+        //为何说配合第三种方法好呢，先用第三种方法创建一个作为Prefab的实体，再通过这种方法大量复制，会很方便
+        //但是要注意，SystemState类型的组件不会被复制
+        //因为官方希望SystemState是在运行时由系统添加的而不是在创建时添加的，具体为什么以后讲
+        //如果希望作为Prefab的实体不参与游戏运行，而像GameObject工作流程中的Prefab一样独立的话
+        //可以在该实体上添加Prefab组件，就不会被任何系统所访问，prefab和disable的特殊地位以后讲
+    }
+    /// <summary>
+    /// 通过EntityManager获取和保存组件的值，这些方法不是很推荐，如在系统中应使用另一套方法
+    /// </summary>
+    /// <param name="e"></param>
+    public void GetAndSet(Entity e)
+    {
+        //获取普通组件，SystemState组件同理
+        TestComponentData data1 = EntityManager.GetComponentData<TestComponentData>(e);
+        data1.Value += 1;
+        //保存组件数据的修改，同理这里的泛型可省略
+        EntityManager.SetComponentData<TestComponentData>(e,data1);
+        //获取共享组件
+        TestSharedComponentData data2 = EntityManager.GetSharedComponentData<TestSharedComponentData>(e);
+        data2.Value += 1;
+        //保存组件数据的修改，注意这里只会使该entity对应另外一个共享组件，而不会修改entity对应共享组件的值
+        EntityManager.SetSharedComponentData(e, data2);
+
+        //获取Buffer组件，修改不需要调用函数保存，会直接保存
+        DynamicBuffer<TestBufferElementData> data3 = EntityManager.GetBuffer<TestBufferElementData>(e);
+        //修改方式已经介绍过了，就不再介绍了
+
+        //获取ComponentObject组件
+        TestClass data4 = EntityManager.GetComponentObject<TestClass>(e);
+        data4.Value += 1;//ComponentObject和Buffer一样是自动保存的
+        
+    }
+    /// <summary>
+    /// Entity的删除
+    /// </summary>
+    public void Delete(Entity e)
+    {
+    //本函数仅供介绍，不准备运行，所以上下文没有关联，每段代码都可以拆出去用，但直接运行会报错，可以猜猜为什么
+        EntityManager.DestroyEntity(e);
+        //删除组件的函数只有一个，什么类型的组件都可以用这个函数删除
+        EntityManager.RemoveComponent<TestComponentData>(e);
+        EntityManager.RemoveComponent<TestSharedComponentData>(e);
+        EntityManager.RemoveComponent<TestSystemStateComponentData>(e);
+        EntityManager.RemoveComponent<TestBufferElementData>(e);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ECSSample/Assets/ECS/Scripts/4ComponentSystem的使用/ComponentSystemCreate.cs
@@ -0,0 +1,123 @@
+﻿using Unity.Entities;
+using Unity.Collections;
+
+//好久不见了，之前忙于学业，现在终于考完了
+//接下来更新频率会高一些
+//之前有人问我上一期视频中教的给class用IComponentData为什么无效，后来我发现应该和Entities装的版本有关，我现在的教程将一直基于最新版本
+//如果是较老的版本可能会无法使用一些API，如果新版本去掉了什么以前我讲过的API的话我也会在最新的视频里提及
+//今天我将讲ECS中的S，如何使用一个System，系统分为三大类，而现在讲的是最基础的一类，ComponentSystem
+
+//本工程文件已开源于https://github.com/ThreeIce/ThreeIceECSSample
+
+
+
+
+//该系统将操作上期视频创建的Entity
+//System不像MonoBehaviour那样，必须要手动拖到一个GameObject上才能运行，启动时unity会通过反射自动把所有的系统添加到默认生成的世界中
+//也可以通过一些代码控制这个过程，但这部分代码不再今天讲的范围内
+//[AlwaysUpdateSystem]这个特性可以让系统即使不满足运行条件也一直运行下去，不被Unity自动暂停
+//[UpdateAfter(xx)][UpdateBefore(xx)]可以控制系统的执行顺序
+//[DisableAutoCreation]可以让系统不在启动时被添加到默认世界中
+public class TestComponentSystem : ComponentSystem
+{
+    /// <summary>
+    /// 用来获取拥有指定组件实体和修改组件值的工具，不过不推荐通过这玩意修改组件值，不直观，为什么接下来你们就知道了
+    /// 同一个系统可以有多个eq，没有限制
+    /// </summary>
+    private EntityQuery eq;
+    /// <summary>
+    /// 该函数会在系统被创建时调用，相当于MonoBehaviour中的Start
+    /// </summary>
+    protected override void OnCreate()
+    {
+        CreateEntityQuery1();
+        SetFilter();
+    }
+    /// <summary>
+    /// 普通eq的创建
+    /// </summary>
+    private void CreateEntityQuery1(){
+        //最基本的创建eq的方法，该函数是系统所具有的，实质是调用EntityManager上的同名函数
+        //意为搜索同时拥有TestComponentData组件和TestSharedComponentData的实体
+        //ReadWrite在这里就有用了，它涉及了系统的调用排序和一系列复杂的依赖关系，总之如果你要修改获取的组件的的值就用ReadWrite，不需要就ReadOnly
+        eq = this.GetEntityQuery(ComponentType.ReadWrite<TestComponentData>(),ComponentType.ReadOnly<TestSharedComponentData>());
+        //标记该EntityQuery是系统执行所依赖的，如果该eq搜索不到拥有目标组件的实体系统就不会执行，省性能
+        this.RequireForUpdate(eq);
+    }
+    /// <summary>
+    /// 复杂eq的创建
+    /// </summary>
+    private void CreateEntityQuery2(){
+        //EntityQuery依赖信息的创建
+        EntityQueryDesc desc = new EntityQueryDesc(){
+            //All内所有的组件实体都必须包含
+            All = new ComponentType[]{ComponentType.ReadWrite<TestComponentData>()},
+            //Any内所有的组件实体必须包含至少一个
+            Any = new ComponentType[]{ComponentType.ReadOnly<TestSharedComponentData>()},
+            //None内所有的组件实体都不能包含（此时ReadOnly和RadWrite区别不大）
+            None = new ComponentType[]{ComponentType.ReadOnly<TestBufferElementData>()}
+        };
+        eq = GetEntityQuery(desc);
+    }
+    /// <summary>
+    /// eq的高级操作，过滤
+    /// </summary>
+    private void SetFilter(){
+        //筛选具有指定组件值的实体的方法
+        //意思是只要TestSharedComponentData的Value为2的组件，如果是1或3都不要，起筛选作用，不过目前似乎只有SharedComponent支持作为筛选依据
+        eq.SetSharedComponentFilter(new TestSharedComponentData(){ Value = 2});//value为1的筛不出来，为什么可以思考一下（滑稽）
+        //筛选组件版本改过的实体，这个版本系统很坑，不仅修改数据会更改，作为ReadWrite访问过也会，使用请慎重，概率筛到目标组件数据没变过的实体
+        //eq.SetChangedVersionFilter(ComponentType.ReadWrite<TestComponentData>());
+    }
+    /// <summary>
+    /// 该函数相当于MonoBehaviour中的Update，每帧调用，默认是和Update同一个时机，不过也可以修改成PrelateUpdate同一个时机等，讲系统组时会讲到
+    /// </summary>
+    protected override void OnUpdate()
+    {
+        Update2();
+    }
+    /// <summary>
+    /// 第一种读取和修改获取的组件的数据的方法
+    /// </summary>
+    private void Update1(){
+        //这串方法贼长而且难以理解，Entities的API挺复杂混乱的，所以我只讲推荐的方法
+        //With传入一个eq，这个类会自动帮你调用eq的获取组件函数并处理后传给你，原理详见创建方法2
+        //foreach的意思是迭代所有匹配到的实体，Entity e这行参数可以删了也可以留着，看你处理时需不需要这个信息了
+        //组件前缀必须用ref，因为是组件是个struct，不用ref你修改的数据没法保存~
+        //该方法只能改IComponentData，不过是同步运行的，用e和EntityManager还是有操作空间的
+        this.Entities.With(eq).ForEach<TestComponentData>((Entity e,ref TestComponentData data)=>{
+            data.Value += 1;
+            //举个操作空间的例子
+            var data2 = EntityManager.GetSharedComponentData<TestSharedComponentData>(e);
+        });
+    }
+    /// <summary>
+    /// 第二种读取和修改获取的组件的数据的方法
+    /// </summary>
+    private void Update2(){
+        //通过eq直接获取搜到的组件的数组
+        //NativeArray是Unity官方推出的非托管的数组，注意非托管数组是手动释放，用完要dispose
+        //位于Unity.Collections命名空间中，相关的NativeContainer类型以后会讲
+        //Allocator标志着这个数组该什么时候释放，如果你没在规定时间内释放会报错提醒你，避免了内存泄露，TempJob意为要在几帧内释放
+        //（当然具体报错时机我也不知道，按照规则用就是了）
+        NativeArray<TestComponentData> dataarray = eq.ToComponentDataArray<TestComponentData>(Allocator.TempJob);
+        //获得entity的数组，该数组的index与dataarray一一对应，
+        NativeArray<Entity> entities = eq.ToEntityArray(Allocator.TempJob);
+        //eq也可以获取ComponentObject，但SharedCompoenntData和buffer只能通过EntityManager获取，如上一个方法里的操作
+        for(int i = 0;i<entities.Length;i++){
+            var e = entities[i];
+            var data = dataarray[i];
+            data.Value += 1;
+            //在ComponentSystem中应使用该方法来保存数据改动，而不是EntityManager，在JobComponentSystem中则是另外一个相似的方法
+            //PostUpdateCommands是一个EntityCommandBuffer对象，可以视为EntityManager的延迟同步版本
+            //有关于EntityCommandBuffer以后会细讲
+            this.PostUpdateCommands.SetComponent(e,data);//上一个数据修改方法也可以用PostUpdateCommands
+        }
+        //还有一种不通过PostUpdateCommands的保存方式，将数据修改存回原本的数组dataarray，然后调用该方法
+        //eq.CopyFromComponentDataArray(dataarray);
+
+        //释放
+        dataarray.Dispose();
+        entities.Dispose();
+    }
+}
